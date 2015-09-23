384c50420f2c667b05abb52617bbdf6b3210eae5
SasquatchShawn <srmiranda1@gmail.com>
2015-09-22 22:01:59 -0400

    Started more tests for dog form.

--- spec/features/user_fills_out_dog_form_spec.rb
+++ spec/features/user_fills_out_dog_form_spec.rb
@@ -4,21 +4,26 @@ feature 'view dog request form', %Q{
   As a user
   I want to visit the main page
   So that I can fill in what kind of dog I want
+
+  Acceptance Criteria
+  [] I can provide a breed
+  [] I can provide a size
+  [] I can provide a need to be good with children
+  [] I can provide an age range
+  [] I can provide a prefered gender
+  [] I can specify if the dog must be fixed
+  [] I must be presented with errors if I fill out the form incorrectly
 } do
-  # Acceptance Criteria
-  #- I can provide a breed
-  #- I can provide a size
-  #- I can provide a need to be good with children
-  #- I can provide an age range
-  #- I can provide a prefered gender
-  #- I can specify if the dog must be fixed
-  #- I must be presented with errors if I fill out the form incorrectly
 
-    scenario 'visitor visits dog request page' do
-      dog = FactoryGirl.create(:dog)
+  scenario 'visitor visits dog request page' do
+    dog = FactoryGirl.create(:dog)
 
-      visit root_path
-      click_link "Find me a dog!"
-      expect(page).to have_content("Dogs Matching:")
-    end
+    visit root_path
+    click_link "Find me a dog!"
+    expect(page).to have_content("Dogs Matching:")
+  end
+
+  scenario 'user fails to fill out form correctly' do
+    click_link "Find me a dog!"
+  end
 end
