2bf5d446360d13215476e839bf5ddb97ee9ddb3c
SasquatchShawn <srmiranda1@gmail.com>
2015-10-13 16:35:22 -0400

    new topbar styling

---
+++ app/assets/javascripts/topbar.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  var active0 = false;
+  var active1 = false;
+  var active2 = false;
+  var active3 = false;
+  var active4 = false;
+
+    $('.parent2').on('mousedown touchstart', function() {
+      
+     if (!active0) $(this).find('.test0').css({'background-color': '#88ABC2', 'transform': 'translate(385px,-25px)'});
+    else $(this).find('.test0').css({'background-color': 'silver', 'transform': 'none'});
+     if (!active1) $(this).find('.test1').css({'background-color': '#88ABC2', 'transform': 'translate(310px,-25px)'});
+    else $(this).find('.test1').css({'background-color': 'silver', 'transform': 'none'});
+     if (!active2) $(this).find('.test2').css({'background-color': '#88ABC2', 'transform': 'translate(235px,-25px)'});
+    else $(this).find('.test2').css({'background-color': 'silver', 'transform': 'none'});
+     if (!active3) $(this).find('.test3').css({'background-color': '#88ABC2', 'transform': 'translate(160px,-25px)'});
+    else $(this).find('.test3').css({'background-color': 'silver', 'transform': 'none'});
+     if (!active4) $(this).find('.test4').css({'background-color': '#88ABC2', 'transform': 'translate(85px,-25px)'});
+    else $(this).find('.test4').css({'background-color': 'silver', 'transform': 'none'});
+    active0 = !active0;
+    active1 = !active1;
+    active2 = !active2;
+    active3 = !active3;
+    active4 = !active4;
+
+    });
+});
