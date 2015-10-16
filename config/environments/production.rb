50a32cc254ff702a9d019237dfda523c213c8f6f
SasquatchShawn <srmiranda1@gmail.com>
2015-10-15 21:45:48 -0400

    Adding forget password mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,23 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.action_mailer.default_url_options = { :host => 'https://iadopt.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
